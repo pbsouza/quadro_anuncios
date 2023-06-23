156120499619966881b7579479710ef2353f6eb2
Pedro Belchior Souza <belchior87@gmail.com>
2023-06-23 10:46:41 -0300

    Alteração do arquivo attributes.txt

--- js/main.js
+++ js/main.js
@@ -9,11 +9,15 @@ async function run() {
 
 // Função para ler o arquivo de atributos
 async function readAttributesFile() {
-    const response = await fetch('data/attributes.txt');
+    const response = await fetch('js/data/attributes.txt');
     const text = await response.text();
-    const attributeNames = text.trim().split('\n');
+    const attributeNames = text.trim().split('\n').map(line => line.trim().split(';')).flat();
+    console.log(attributeNames)
     return attributeNames;
   }
+  
+  
+  
   
   // Chama a função para ler o arquivo e processar os atributos
   run().then(dataTable => {
